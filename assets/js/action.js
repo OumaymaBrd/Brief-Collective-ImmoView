a1c7b13e8dc24e2a63ee2d1cfa5aa18e36c38bbc
abdel44578 <abdel.elmalky@gmail.com>
2024-10-25 19:05:38 +0100

    ajout de la responsivité

--- assets/js/action.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.getElementById('burger-menu');
-    const navLinks = document.getElementById('nav-links');
-
-    burgerMenu.addEventListener('click', () => {
-        navLinks.classList.toggle('active'); // Toggle the 'active' class
-    });
